37a249111097d90603bbba247918e59f8426856e
João Victor <reflixplayshd@gmail.com>
2022-07-01 19:59:47 -0300

    Contact, var, env, tostify, email form

--- src/index.js
+++ src/index.js
@@ -3,12 +3,26 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { ContextProvider } from './context/ContextProvider';
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <ContextProvider>
     <React.StrictMode>
       <App className="App" />
+      <ToastContainer
+        position="bottom-right"
+        autoClose={1500}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        toastStyle={{ backgroundColor: '#0c0f12', color: '#858585' }}
+      />
     </React.StrictMode>
   </ContextProvider>
 );
