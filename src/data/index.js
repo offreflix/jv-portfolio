79a4ec2389b3e43422cb79461f56330ed81051af
João Victor <reflixplayshd@gmail.com>
2022-07-06 13:06:26 -0300

    Update index.js

--- src/data/index.js
+++ src/data/index.js
@@ -24,41 +24,4 @@ export const projects = [
       'https://cdn.discordapp.com/attachments/958055531774361630/989563861315248218/unknown.png',
     id: 2,
   },
-  {
-    name: 'JunkBlog',
-    slug: 'https://graphql-nextjs-blog.vercel.app/',
-    githubSlug: 'https://github.com/Reflixzinhu/graphql-nextjs-blog',
-    technologies: ['Next.Js', 'React.Js', 'GraphQl', 'Tailwind CSS'],
-    description:
-      'Um blog com posts recentes, categorias, comentários, informação do autor e artigos usando Markdown.',
-    about: '',
-    image:
-      'https://media.discordapp.net/attachments/958055531774361630/989601811637895198/unknown.png',
-    id: 3,
-  },
-  {
-    name: 'JM Store',
-    slug: 'https://jm-store.vercel.app/',
-    githubSlug: 'https://github.com/Reflixzinhu/ecommerce-stripe',
-    technologies: ['Next.Js', 'React.Js', 'Sanity', 'Stripe', 'ContextAPI'],
-    description: 'Um aplicação de E-commerce com pagamentos utilizando Stripe.',
-    about: '',
-    image:
-      'https://cdn.discordapp.com/attachments/958055531774361630/989563861315248218/unknown.png',
-    id: 4,
-  },
-  {
-    name: 'JunkBlog',
-    slug: 'https://graphql-nextjs-blog.vercel.app/',
-    githubSlug: 'https://github.com/Reflixzinhu/graphql-nextjs-blog',
-    technologies: ['Next.Js', 'React.Js', 'GraphQl', 'Tailwind CSS'],
-    description:
-      'Um blog com posts recentes, categorias, comentários, informação do autor e artigos usando Markdown.',
-    about: '',
-    image:
-      'https://media.discordapp.net/attachments/958055531774361630/989601811637895198/unknown.png',
-    id: 5,
-  },
 ];
-
-// export const pages = ['about', 'contact', 'projects', 'resume', 'skills'];
